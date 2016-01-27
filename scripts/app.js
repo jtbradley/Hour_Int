ce2536384cc38a8e28d2012b0555a449e1fbbdc6
Jtbradley <jtbradley1@gmail.com>
2016-01-27 14:06:28 -0800

    launch commit 1.27.16

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+﻿
+// toggle black content boxes visibility
+$('.blackbox2').hide();
+$('.projects-stack').show('slow');
+
+$('.about-btn').click(function () {
+    $('.about-stack').toggle('slow');
+    $('.contact-stack').hide('slow');
+    $('.projects-stack').hide('slow');
+
+});
+
+$('.contact-btn').click(function () {
+    $('.contact-stack').toggle('slow');
+    $('.about-stack').hide('slow');
+    $('.projects-stack').hide('slow');
+});
+
+$('.projects-btn').click(function () {
+    $('.projects-stack').toggle('slow');
+    $('.contact-stack').hide('slow');
+    $('.about-stack').hide('slow');
+}); 
+
+/*
+$('ol.links > .btn').click(function() {
+    if ($('this').prop("")
+    $(this).css("color", "red");
+});*/
